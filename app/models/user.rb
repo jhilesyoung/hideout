a735d2bb672d171ea363fb329877c5ced96ddafe
jhilesyoung <jhilesyoung@gmail.com>
2020-11-17 11:53:11 -0500

    heroku maintenance part 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :session_token, presence: true, uniqueness: true
-    validates :password, presence: true length: { minimum: 6, allow_nil: true }
-    validates password_digest, presence: true
+    validates :password, presence: true, length: { minimum: 6, allow_nil: true }
+    validates :password_digest, presence: true
 
     after_initialize :ensure_session_token
 
